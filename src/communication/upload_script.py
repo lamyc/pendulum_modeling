5c873b7eb60b86c09fffc908c19322ab31dc9d4d
lamyc <lamyc627@gmail.com>
2021-06-12 00:12:57 +0800

    added upload function from python

--- src/communication/upload_script.py
+++ src/communication/upload_script.py
@@ -1,1 +1,19 @@
-import os, sys
+import subprocess
+import os
+
+def upload(file_path, port, timeout=1):
+    ret = subprocess.run(["arduino","--upload", "--port", port, "-v",file_path],
+                         shell=True,
+                         encoding="utf-8",
+                         timeout=timeout)
+
+    if ret.returncode == 0:
+        print("success:",ret)
+    else:
+        print("error:",ret)
+
+if __name__ == "__main__":
+    fpath = os.path.join("..", "..", "nano_33", "script", "ReadAccelerometer",
+            "ReadAccelerometer.ino")
+    upload(fpath, "/dev/ttyS4")
+
