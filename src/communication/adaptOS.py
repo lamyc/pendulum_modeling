67dd9881b5d3be08bfc3c5dc61dc691adb51028c
Lam Yee Ching <lamyc627@gmail.com>
2021-06-12 16:07:54 +0800

    auto resolve nanp's port

--- src/communication/adaptOS.py
+++ src/communication/adaptOS.py
@@ -1,2 +1,21 @@
-import sys
+import warnings
+import serial
+import serial.tools.list_ports
 
+def get_nano_port():
+    arduino_ports = [
+            p.device
+            for p in serial.tools.list_ports.comports()
+            if 'Nano' in p.description
+    ]
+
+    if not arduino_ports:
+        raise IOError("No Arduino found")
+    if len(arduino_ports) > 1:
+        warnings.warn("Multiple Arduinos found - using the first")
+
+    return arduino_ports[0]
+
+if __name__ == "__main__":
+    print(get_nano_port())
+    pass
