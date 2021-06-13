197928bb678879e07b2e7b988923ff6de5f29ee2
lamyc <lamyc627@gmail.com>
2021-06-13 21:03:44 +0800

    -

--- nano_33/script/print/print.ino
+++
@@ -1,9 +0,0 @@
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(2000000);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("123456789012345678901234567890123456");
-}
